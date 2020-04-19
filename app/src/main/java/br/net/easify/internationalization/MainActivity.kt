0e8094101b822b843882270e0288469b79eea642
viniciuspontes <vipontes70@gmail.com>
2020-04-19 10:29:19 -0300

    first commit

---
+++ app/src/main/java/br/net/easify/internationalization/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.net.easify.internationalization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * @author Vinicius Pontes
+ *
+ * https://developer.android.com/guide/topics/resources/localization
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
